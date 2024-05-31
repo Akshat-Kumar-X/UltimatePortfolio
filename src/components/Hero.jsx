5c960159e843477af203121ae55775d6439824a8
Akshat Kumar <akshatkumar10203@gmail.com>
2024-05-31 22:19:12 +0530

    hero change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Akshat</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop web applications & <br className='sm:block hidden' />
+            Solutions for Real World Problems.
           </p>
         </div>
       </div>
